55f403fa4b2485452fe579ccb8e8107afae56239
Eduardo Romo <eduromogo@outlook.es>
2016-12-10 21:44:10 +0100

    added webpack configuration for testing

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,53 @@
+// module.exports = {
+//     output: {
+//         path: './static/js/build',
+//         filename: 'app.bundle.js',
+//     },
+//     module: {
+//         loaders: [{
+//             test: /\.js$/,
+//             exclude: /node_modules/,
+//             loader: 'babel-loader'
+//         }]
+//     }
+// }
+
+const path = require('path');
+const webpack = require('webpack');
+
+// env
+const buildDirectory = './static/js/build';
+
 module.exports = {
     entry: './static/js/src/index.js',
+    devServer: {
+        hot: true,
+        inline: true,
+        port: 7700,
+        historyApiFallback: true,
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx'],
+    },
     output: {
-        path: './static/js/build',
-        filename: 'app.bundle.js',
+        path: path.resolve(buildDirectory),
+        filename: 'app.js',
+        publicPath: 'http://localhost:7700/dist',
+    },
+    externals: {
+        'cheerio': 'window',
+        'react/lib/ExecutionEnvironment': true,
+        'react/lib/ReactContext': true,
     },
     module: {
         loaders: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader'
-        }]
-    }
-}
+            test: /\.jsx?$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel',
+            query: {
+                presets: ['react', 'es2015', 'stage-0'],
+            },
+        }],
+    },
+    plugins: [],
+};
